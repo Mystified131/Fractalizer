c6c00077675446870604865fc40840c58a5b7580
Thomas Park <mystifiedthomas@gmail.com>
2021-08-25 23:02:37 -0500

    tweaking

--- SpeakInParagraphsWorldFact.py
+++ SpeakInParagraphsWorldFact.py
@@ -188,28 +188,36 @@ for citr in range(8):
         print(error)
         sys.exit(-1)
 
-    # Access the audio stream from the response
-    if "AudioStream" in response:
-        # Note: Closing the stream is important because the service throttles on the
-        # number of parallel connections. Here we are using contextlib.closing to
-        # ensure the close method of the stream object will be called automatically
-        # at the end of the with statement's scope.
-            with closing(response["AudioStream"]) as stream:
-                #output = os.path.join(gettempdir(), outaud)
-                output = ("C:\\Users\\mysti\\Coding\\Fractalizer\\" +  outaud)
+    try:
+        # Access the audio stream from the response
+        # if "AudioStream" in response:
+            # Note: Closing the stream is important because the service throttles on the
+            # number of parallel connections. Here we are using contextlib.closing to
+            # ensure the close method of the stream object will be called automatically
+            # at the end of the with statement's scope.
+        with closing(response["AudioStream"]) as stream:
+            #output = os.path.join(gettempdir(), outaud)
+            output = ("C:\\Users\\mysti\\Coding\\Fractalizer\\" +  outaud)
 
-                try:
-                # Open a file for writing the output as a binary stream
-                    with open(output, "wb") as file:
-                        file.write(stream.read())
-                except IOError as error:
-                # Could not write to file, exit gracefully
-                    print(error)
-                    sys.exit(-1)
+        try:
+        # Open a file for writing the output as a binary stream
+            with open(output, "wb") as file:
+                file.write(stream.read())
+        except IOError as error:
+        # Could not write to file, exit gracefully
+                print(error)
+                sys.exit(-1)
+
+    except:
+        print("")
+        print("Error processing speech.")
+        print("")
 
     else:
         # The response didn't contain audio data, exit gracefully
+        print("")
         print("Could not stream audio")
+        print("")
         sys.exit(-1)
 
     # Play the audio using the platform's default player
